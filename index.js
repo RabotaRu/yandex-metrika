dcc0a0c0abb98aa1411b61c334b32a69d07eff3c
Alexander Belov <asb@rdw.ru>
2019-02-14 16:04:31 +0300

    Commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ export default function yandexMetrika (moduleOptions) {
   // Add yandex metrika script to head
   this.options.head.script.push({
     src: 'https://mc.yandex.ru/metrika/tag.js', // https://mc.yandex.ru/metrika/watch.js',
-    async: 1
+    async: ''
   });
 
   // register plugin
