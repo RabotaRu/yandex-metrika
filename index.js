f3a436b4b44d218c12d291ceccebf0924678d01c
IPRIT <ipritoflex@yandex.ru>
2019-02-17 15:56:30 +0300

    Update

--- index.js
+++ index.js
@@ -6,13 +6,22 @@ export default function yandexMetrika (moduleOptions) {
     return;
   }
 
-  const useCDN = moduleOptions.cdn;
+  const {
+    useCDN = false,
+    noscript = true,
+    counter
+  } = moduleOptions;
+
   const libURL = !useCDN
     ? 'https://mc.yandex.ru/metrika/tag.js'
     : 'https://cdn.jsdelivr.net/npm/yandex-metrica-watch/tag.js';
 
-  // include metrika script content
-  const metrikaContent = `
+  const isDynamicCounter = typeof counter === 'function';
+  const includeInitContent = counter && !isDynamicCounter;
+  const includeNoscript = noscript && counter && !isDynamicCounter;
+
+  // yandex metrika init script
+  let metrikaContent = `
     (function(m,e,t,r,i,k,a){
      m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
      m[i].l=1*new Date();
@@ -20,12 +29,36 @@ export default function yandexMetrika (moduleOptions) {
     (window, document, "script", "${libURL}", "ym");
   `;
 
+  // main counter init script
+  const metrikaInitContent = `
+    ym(${counter}, "init", {
+      id:52381765,
+      clickmap:true,
+      trackLinks:true,
+      accurateTrackBounce:true
+    });
+  `;
+
+  if (includeInitContent) {
+    metrikaContent += metrikaInitContent;
+  }
+
+  const noscriptContent = `
+    <div><img src="https://mc.yandex.ru/watch/${counter}" style="position:absolute; left:-9999px;" alt="" /></div>
+  `;
+
   this.options.head.__dangerouslyDisableSanitizers = [ 'script' ];
-  this.options.head.script.unshift({
+  this.options.head.script = [].concat( this.options.head.script || [] ).unshift({
     innerHTML: metrikaContent,
     type: 'text/javascript',
     charset: 'utf-8'
   });
+
+  if (includeNoscript) {
+    this.options.head.noscript = [].concat( this.options.head.noscript || [] ).unshift({
+      innerHTML: noscriptContent
+    });
+  }
 
   // register plugin
   this.addPlugin({
--- plugin.js
+++ plugin.js
@@ -8,43 +8,55 @@ export default async (context, inject) => {
   }
 
   const pluginOptions = <%= serialize(options) %>;
-  const counters = await resolveCounters( pluginOptions.counters, context );
 
   const { app: { router } } = context;
-  const { options = {} } = pluginOptions;
+  let {
+    counter,
+    includeCounters = [],
+    options = {}, // yandex metrika options
+    logging = false
+  } = pluginOptions;
 
-  const yandexLayer = new YandexLayer( counters );
-
-  // init each yandex counter
-  counters.forEach(counterId => {
-    options.id = counterId;
-
-    yandexLayer.pushTo( counterId, 'init', options );
+  // resolve all counters
+  includeCounters = await resolveCounters( includeCounters, context );
+  counter = await resolveCounters( counter, context ).then(counters => {
+    return counters && counters.length && counters[ 0 ];
   });
+
+  const layer = new YandexLayer({
+    counter, includeCounters, logging, options
+  });
+
+  layer.init( includeCounters );
 
   router.afterEach((to, from) => {
     const fromPath = from.fullPath;
     const toPath = to.fullPath;
 
     // send new page url with the referer to each counter
-    yandexLayer.pushAll('hit', toPath, {
+    layer.pushAll('hit', toPath, {
       referer: fromPath
     });
   });
 
   // inject yandex metrika layer into context
-  inject( 'ym', yandexLayer );
+  inject( 'ym', layer );
 }
 
 /**
  * @param {number|string|Array<number|string>|Function} countersOrFn
  * @param {*} context
- * @return {Promise<*>}
+ * @return {Promise<Array<string|number>>}
  */
 async function resolveCounters (countersOrFn, context) {
-  if (typeof countersOrFn === 'function') {
-    return countersOrFn( context );
-  }
+  const useFn = typeof countersOrFn === 'function';
+  const wrappedFn = Promise.resolve(
+    useFn
+      ? countersOrFn( context )
+      : countersOrFn
+  );
 
-  return [].concat( countersOrFn || [] );
+  return wrappedFn.then(counters => {
+    return [].concat( counters || [] );
+  });
 }
